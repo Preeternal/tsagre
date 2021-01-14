8e294c044f21e45378af3cf7272b5fe68a57ba36
Preeternal <preeternal@ya.ru>
2021-01-14 14:17:53 +0200

    continue with persist cache

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {AppRegistry} from 'react-native';
+import {ApolloProvider} from '@apollo/client';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import App from './App';
+import {client, apolloPersistor} from './src/api/client';
 import {name as appName} from './app.json';
 import {store, persistor} from './src/state/store';
 import {Loader} from './src/components/common/Loader/Loader';
 
-const Start = () => (
-  <Provider store={store}>
-    <PersistGate persistor={persistor} loading={<Loader />}>
-      <App />
-    </PersistGate>
-  </Provider>
-);
+const Start = () => {
+  useEffect(() => {
+    async function restore() {
+      await apolloPersistor.restore();
+      apolloPersistor.getLogs(true);
+    }
+    restore();
+  }, []);
+  return (
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={<Loader />}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </ApolloProvider>
+  );
+};
 
 AppRegistry.registerComponent(appName, () => Start);
--- src/api/client.js
+++ src/api/client.js
@@ -1,11 +1,23 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 import {RestLink} from 'apollo-link-rest';
+import AsyncStorage from '@react-native-community/async-storage';
+import {CachePersistor, AsyncStorageWrapper} from 'apollo3-cache-persist';
 
 import {apiUrl} from '../config';
+
+const cache = new InMemoryCache();
+
+export const apolloPersistor = new CachePersistor({
+  cache,
+  storage: new AsyncStorageWrapper(AsyncStorage),
+  debug: __DEV__,
+  trigger: 'write',
+});
 
 const restLink = new RestLink({uri: apiUrl});
 
 export const client = new ApolloClient({
-  cache: new InMemoryCache(),
+  // cache: new InMemoryCache(),
+  cache,
   link: restLink,
 });
