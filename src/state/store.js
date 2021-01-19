b26432f3f779792fcb10647527c036f16ee2943d
Preeternal <preeternal@ya.ru>
2021-01-19 15:32:47 +0200

    start with sagas

--- src/state/store.js
+++ src/state/store.js
@@ -3,8 +3,10 @@ import {persistStore, persistReducer} from 'redux-persist';
 import AsyncStorage from '@react-native-community/async-storage';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import {rootReducer} from '.';
+import mySaga from './sagas';
 
 const persistConfig = {
   key: 'root',
@@ -13,11 +15,14 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   persistedReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
+  composeWithDevTools(applyMiddleware(thunk, sagaMiddleware)),
 );
+
+sagaMiddleware.run(mySaga);
 
 const persistor = persistStore(store);
 
